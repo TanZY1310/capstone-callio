cad51242911f0587d403adfbe2ff25133ded90ee
TanZY1310 <90238761+TanZY1310@users.noreply.github.com>
2026-05-23 15:35:35 +0800

    Update sidebar-navbar Add Lucide Icons

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,9 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
 import "./App.css";
 import Sidebar from "./components/Layout/Sidebar";
 import { BrowserRouter } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
