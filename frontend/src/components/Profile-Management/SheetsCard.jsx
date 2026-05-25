3dac58dd4402d4e830d7ef704d2df40f37aa7c1b
IzzatFarhan <izzatfrhann@gmail.com>
2026-05-25 09:56:21 +0800

    refine textfield

--- frontend/src/components/Profile-Management/SheetsCard.jsx
+++ frontend/src/components/Profile-Management/SheetsCard.jsx
@@ -1,14 +1,14 @@
 function SheetsCard() {
   return (
     <div>
-      <fieldset className="$$fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
-        <legend className="$$fieldset-legend">Google Sheets API</legend>
+      <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+        <legend className="fieldset-legend">Google Sheets API</legend>
 
-        <label className="$$label">API Key</label>
-        <input type="text" class="$$input" placeholder="API Key" />
+        <label className="label">API Key</label>
+        <input type="text" class="input" placeholder="API Key" />
 
-        <label className="$$label">Spreadsheet ID</label>
-        <input type="text" class="$$input" placeholder="Spreadsheet ID" />
+        <label className="label">Spreadsheet ID</label>
+        <input type="text" class="input" placeholder="Spreadsheet ID" />
 
         <button className="btn btn-neutral">🤝Sync Now</button>
       </fieldset>
--- frontend/src/components/Profile-Management/SocialCard.jsx
+++ frontend/src/components/Profile-Management/SocialCard.jsx
@@ -1,18 +1,14 @@
 function SocialCard() {
   return (
     <div>
-      <fieldset className="$$fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
-        <legend className="$$fieldset-legend">WhatsApp Business API</legend>
+      <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+        <legend className="fieldset-legend">WhatsApp Business API</legend>
 
-        <label className="$$label">Account ID</label>
-        <input
-          type="text"
-          className="$$input"
-          placeholder="Enter Business ID"
-        />
+        <label className="label">Account ID</label>
+        <input type="text" className="input" placeholder="Enter Business ID" />
 
-        <label className="$$label">Message Template</label>
-        <input type="text" className="$$input" placeholder="" />
+        <label className="label">Message Template</label>
+        <input type="text" className="input" placeholder="" />
 
         <button className="btn btn-success">⛓️‍💥Link Account</button>
       </fieldset>
