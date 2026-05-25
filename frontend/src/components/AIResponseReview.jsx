6aea38ddd0a93816d1100cb75f9dcc2e06acd621
Tan Ze Yan <tanzy1310@gmail.com>
2026-05-25 04:41:01 +0000

    Revert "Merge branch 'feature/whatsapp' into 'main'"
    This reverts merge request !5

--- frontend/src/components/AIResponseReview.jsx
+++
@@ -1,9 +0,0 @@
-function AIResponseReview(){
-  {/*ask AI how to code this beforehand, on stuff I can code pre-backend linking*/}
-
-    return(
-    <>
-    </>);
-}
-
-export default AIResponseReview;
--- frontend/src/components/ConvoHistory.jsx
+++
@@ -1,9 +0,0 @@
-function ConvoHistory(){
-  {/*ask AI how to code this beforehand, on stuff I can code pre-backend linking*/}
-
-    return(
-    <>
-    </>);
-}
-
-export default ConvoHistory;
