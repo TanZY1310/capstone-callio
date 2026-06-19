84028b7063a784f0f57232257c3b58ffcba92b02
TanZY1310 <90238761+TanZY1310@users.noreply.github.com>
2026-06-19 21:30:51 +0800

    Add audio player, remove models.py from root, Add router for speech

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import Session
 from pydantic import BaseModel, StrictInt, Field
 
 #Instead of importing routers.customer i can just from routers with the use of __init__.py
-from routers import customers, auth, sheets, user_profile, whatsapp
+from routers import customers, auth, sheets, user_profile, whatsapp, speech
 
 app = FastAPI()
 create_tables()
@@ -28,3 +28,4 @@ app.include_router(auth.router)
 app.include_router(whatsapp.router)
 app.include_router(sheets.router)
 app.include_router(user_profile.router)
+app.include_router(speech.router)
